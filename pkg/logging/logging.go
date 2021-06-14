559e3a4d625d63d463d0fc95a063661457055594
Ivank <vannsha.may@gmail.com>
2021-06-14 10:26:49 +0300

    update logger

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -13,18 +13,18 @@ var (
 func Init() *log.Logger {
 	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
 
-	err := os.MkdirAll("logs", 0755)
+	// err := os.MkdirAll("logs", 0755)
 
-	if err != nil || os.IsExist(err) {
-		panic("can't create log dir. no configured logging to files")
-	} else {
-		openLogfile, err := os.OpenFile("logs/info.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
-		if err != nil {
-			log.Fatalf("error opening file: %v", err)
-		}
-		CommonLog = log.New(openLogfile, "Common Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
-		ErrorLog = log.New(openLogfile, "Error Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
-		return log.New(openLogfile, "Default Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
-	}
+	// if err != nil || os.IsExist(err) {
+	// 	panic("can't create log dir. no configured logging to files")
+	// } else {
+	// 	openLogfile, err := os.OpenFile("logs/info.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	// 	if err != nil {
+	// 		log.Fatalf("error opening file: %v", err)
+	// 	}
+	CommonLog = log.New(os.Stdout, "Common Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLog = log.New(os.Stdout, "Error Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(os.Stdout, "Default Logger:\t", log.Ldate|log.Ltime|log.Lshortfile)
+	// }
 
 }
